dc199d6690f09784e0f5e07ca14936aa4588b29b
Faisal Abid <faisal.abid@gmail.com>
2012-08-05 20:03:20 -0400

    Initial Commit

---
+++ LearningCPlusPlus/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LearningCPlusPlus
+//
+//  Created by Faisal Abid on 2012-08-05.
+//  Copyright (c) 2012 Faisal Abid. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
